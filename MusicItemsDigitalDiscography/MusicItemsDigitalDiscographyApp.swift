36d11b077f45af9101226a9efd510b8100a07ed4
Nazar Bahatchenko <bahatchenko.nazar@gmail.com>
2024-04-20 09:37:53 +0300

    Initial Commit

---
+++ MusicItemsDigitalDiscography/MusicItemsDigitalDiscographyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicItemsDigitalDiscographyApp.swift
+//  MusicItemsDigitalDiscography
+//
+//  Created by Nazar Bahatchenko on 20.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct MusicItemsDigitalDiscographyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
