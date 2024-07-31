dfd480683429e6898a028f399b1c5e563527c082
Nazar Bahatchenko <bahatchenko.nazar@gmail.com>
2024-07-31 13:31:29 +0300

    Refactor AddMusicItemView

--- MusicItemsDigitalDiscography/Views/Main Views/AddMusicItemView.swift
+++ MusicItemsDigitalDiscography/Views/Main Views/AddMusicItemView.swift
@@ -18,19 +18,13 @@ struct AddMusicItemView: View {
     @State private var selectedStyle: String = ""
     @State private var barcode: String = ""
     @State private var catno: String = ""
-    @State private var coverImageData: Data? = nil
-    
+    @State private var coverImageData: Data?
     @State private var showingAlert = false
     @State private var alertMessage: String = ""
-    @State private var isButtonEnabled = true
-    
     @ObservedObject var addMusicViewModel: FirestoreViewModel
     @EnvironmentObject var authenticationViewModel: AuthenticationViewModel
-    
     @Environment(\.presentationMode) var presentationMode
-    
     @State private var formIncompleteAlert = false
-    
     var body: some View {
         ZStack {
             Color("MainColor").ignoresSafeArea(.all)
@@ -38,9 +32,9 @@ struct AddMusicItemView: View {
                 Spacer(minLength: 20)
                 VStack {
                     HStack {
-                        ImagePicker(imageData: $coverImageData)
+                        ImagePickerView(imageData: $coverImageData)
                             .frame(width: 200, height: 200)
-                            .cornerRadius(15)
+                            .cornerRadius(16)
                             .padding(.all)
                         VStack(alignment: .leading) {
                             Text("*Ideally Cover Image has to have 1:1 aspect ratio")
@@ -64,23 +58,17 @@ struct AddMusicItemView: View {
                         CustomTextFieldView(text: $catno, textFieldTitle: "Catalog Number")
                     }
                     Spacer(minLength: 50)
-                    
                     if formIncompleteAlert {
                         Text("Please fill all required fields")
                             .font(.caption)
                             .foregroundColor(.red)
                             .padding(.bottom, 5)
                     }
-                    CustomButtonFullScreen(action: {
+                    CustomActionButton(action: {
                         Task {
-                            await addMusicItem()
-                        }
-                        isButtonEnabled = false
-                        Timer.scheduledTimer(withTimeInterval: 5, repeats: false) { _ in
-                            isButtonEnabled = true
-                        }
-                    }, buttonText: "Add New Item")
-                    .disabled(!isButtonEnabled)
+                            await addMusicItemWithFormValidation()
+                        }},
+                        buttonText: "Add New Item")
                 }
                 .padding(.horizontal)
             }
@@ -90,73 +78,29 @@ struct AddMusicItemView: View {
             self.hideKeyboard()
         }
     }
-    struct ImagePicker: View {
-        @Binding var imageData: Data?
-        @State private var pickerItem: PhotosPickerItem? = nil
-        
-        var body: some View {
-            PhotosPicker(selection: $pickerItem, matching: .images) {
-                VStack {
-                    if let imageData = imageData, let image = UIImage(data: imageData) {
-                        Image(uiImage: image)
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 200, height: 200)
-                            .cornerRadius(15)  // Ensure the image also has rounded corners if desired
-                    } else {
-                        RoundedRectangle(cornerRadius: 15)
-                            .fill(Color("AccentColorSecondary").opacity(0.1))
-                            .frame(width: 200, height: 200)
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 15) // Inner rectangle for the stroke
-                                    .stroke(Color("TextColor"), lineWidth: 2)
-                            )
-                            .overlay(
-                                Text("Select Image")
-                                
-                            )
-                    }
-                    
-                }
-            }
-            .onChange(of: pickerItem) { newItem in
-                newItem?.loadTransferable(type: Data.self) { result in
-                    switch result {
-                    case .success(let data): self.imageData = data
-                    case .failure: print("Failed to load image.")
-                    }
-                }
-            }
-        }
-    }
-    
-    func triggerHapticFeedback() {
+}
+
+extension AddMusicItemView {
+    private func triggerHapticFeedback() {
         let generator = UINotificationFeedbackGenerator()
         generator.notificationOccurred(.error)
     }
-    
-    private func addMusicItem() async {
+    private func addMusicItemWithFormValidation() async {
         guard let ownerUID = authenticationViewModel.currentUserUID else {
             alertMessage = "Error: User not logged in"
             showingAlert = true
             return
         }
-        
         if title.isEmpty || year.isEmpty || country.isEmpty {
             alertMessage = "Please fill all required fields"
             showingAlert = true
             return
         }
-        
-        // Directly pass non-optional values assuming you handle empty as non-entry in Firestore or your business logic
-        await addMusicViewModel.addMusicItemWithImage(ownerUID: ownerUID, title: title, year: year, country: country, label: label, genre: selectedGenre, style: selectedStyle, barcode: barcode, catno: catno, imageData: coverImageData, isPublic: false, inTrash: false)
-        
+        await addMusicViewModel.addMusicItemWithImage(ownerUID: ownerUID, title: title, year: year, country: country, label: label,
+                                                      genre: selectedGenre, style: selectedStyle, barcode: barcode, catno: catno, imageData: coverImageData, isPublic: false, inTrash: false)
         presentationMode.wrappedValue.dismiss()
     }
-}
-
-extension View {
-    func hideKeyboard() {
+    private func hideKeyboard() {
         UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
