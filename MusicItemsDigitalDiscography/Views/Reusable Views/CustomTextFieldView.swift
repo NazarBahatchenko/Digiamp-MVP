5b863ca2d62d480dd3f35e353c3f70a55b103343
Nazar Bahatchenko <bahatchenko.nazar@gmail.com>
2024-07-31 13:31:41 +0300

    Add CustomTextField

--- MusicItemsDigitalDiscography/Views/Reusable Views/CustomTextFieldView.swift
+++ MusicItemsDigitalDiscography/Views/Reusable Views/CustomTextFieldView.swift
@@ -13,31 +13,51 @@ struct CustomTextFieldView: View {
     var isNumeric: Bool = false
     var textFieldForegroundText: String = ""
     var textFieldHeight: CGFloat = 40
+    
     var body: some View {
-        
         VStack(alignment: .leading) {
-            
             Text(textFieldTitle)
                 .font(.custom("Supreme-Medium", size: 14))
                 .foregroundColor(Color("TextColor"))
-
-            TextField(textFieldForegroundText, text: $text)
-                .padding(.leading)
-                .font(.custom("Poppins-Regular", size: 14))
-                .frame(height: textFieldHeight)
-                .background(Color("MainColor"))
-                .foregroundColor(Color("AccentColor"))
-                .cornerRadius(8)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 8)
-                        .stroke(Color("AccentColor"), lineWidth: 0.5)
-                )
-                .keyboardType(isNumeric ? .numberPad : .default)
-                .onReceive(text.publisher.collect()) {
-                    if isNumeric {
-                        self.text = String($0.filter { "0123456789".contains($0) })
+            
+            VStack {
+                HStack {
+                    if textFieldHeight > 40 {
+                        TextEditor(text: $text)
+                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                            .background(Color("MainColor"))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 8)
+                                    .stroke(Color("AccentColor"), lineWidth: 0.5)
+                            )
+                            .font(.custom("Poppins-Regular", size: 14))
+                            .foregroundColor(Color("AccentColor"))
+                            .frame(height: textFieldHeight)
+                    } else {
+                        TextField(textFieldForegroundText, text: $text)
+                            .padding(8) // Padding inside the TextField for user input
+                            .background(
+                                RoundedRectangle(cornerRadius: 8)
+                                    .fill(Color("MainColor"))
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 8)
+                                            .stroke(Color("AccentColor"), lineWidth: 0.5)
+                                    )
+                            )
+                            .font(.custom("Poppins-Regular", size: 14))
+                            .foregroundColor(Color("AccentColor"))
+                            .frame(height: textFieldHeight)
+                            .keyboardType(isNumeric ? .numberPad : .default)
+                            .onReceive(text.publisher.collect()) {
+                                if isNumeric {
+                                    self.text = String($0.filter { "0123456789".contains($0) })
+                                }
+                            }
                     }
+                    Spacer()
                 }
+                Spacer()
+            }
         }
         .padding(.horizontal)
     }
